25714f0508ec91d272a900529ef1fc681f42e269
GabrielCriste <129788151+GabrielCriste@users.noreply.github.com>
2025-01-18 10:25:38 -0300

    Update __init__.py

--- jupyter_remote_desktop_proxy/__init__.py
+++ jupyter_remote_desktop_proxy/__init__.py
@@ -1,17 +1,54 @@
 import os
+import shlex
+from shutil import which
+import tempfile
 
-from .server_extension import load_jupyter_server_extension
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
+def setup_desktop():
+    # make a secure temporary directory for sockets
+    # This is only readable, writeable & searchable by our uid
+    sockets_dir = tempfile.mkdtemp()
+    sockets_path = os.path.join(sockets_dir, 'vnc-socket')
+    vncserver = which('vncserver')
 
-def _jupyter_server_extension_points():
-    """
-    Set up the server extension for collecting metrics
-    """
-    return [{"module": "jupyter_remote_desktop_proxy"}]
+    if vncserver:
+        vnc_args = [
+            vncserver,
+        ]
+        socket_args = []
+    else:
+        # Use bundled tigervnc
+        vnc_args = [
+            os.path.join(HERE, 'share/tigervnc/bin/vncserver'),
+            '-rfbunixpath', sockets_path,
+        ]
+        socket_args = [
+            '--unix-target', sockets_path
+        ]
 
-
-# For backward compatibility
-_load_jupyter_server_extension = load_jupyter_server_extension
-_jupyter_server_extension_paths = _jupyter_server_extension_points
+    vnc_command = ' '.join(shlex.quote(p) for p in (vnc_args + [
+        '-verbose',
+        '-xstartup', os.path.join(HERE, 'share/xstartup'),
+        '-geometry', '1680x1050',
+        '-SecurityTypes', 'None',
+        '-fg',
+        ':1',
+    ]))
+    return {
+        'command': [
+            'websockify', '-v',
+            '--web', os.path.join(HERE, 'share/web/noVNC-1.2.0'),
+            '--heartbeat', '30',
+            '5901',
+        ] + socket_args + [
+            '--',
+            '/bin/sh', '-c',
+            f'cd {os.getcwd()} && {vnc_command}'
+        ],
+        'port': 5901,
+        'timeout': 30,
+        'mappath': {'/': '/vnc_lite.html'},
+        'new_browser_window': True
+    }
