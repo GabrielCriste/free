3815f2fa29d0d40b433d2648f69b7447a8a26945
GabrielCriste <129788151+GabrielCriste@users.noreply.github.com>
2025-02-09 13:04:29 -0300

    Update jupyterhub_config.py

--- jupyter-config/jupyter_hub_config.d/jupyterhub_config.py
+++ jupyter-config/jupyter_hub_config.d/jupyterhub_config.py
@@ -1,5 +1,3 @@
-# Configurações de tempo limite
-c.Spawner.http_timeout = 14400  # Tempo de 4 horas antes de encerrar
-c.JupyterHub.shutdown_on_logout = False  # Não encerra a sessão ao fazer logout
-c.ConfigurableHTTPProxy.keep_alive_timeout = 14400  # Proxy ativo por 4 horas
-c.NotebookApp.shutdown_no_activity_timeout = 0  # Desativa encerramento por inatividade
+c.JupyterHub.ip = '0.0.0.0'  # Permitir conexões externas
+c.JupyterHub.port = 8000  # Porta do JupyterHub
+c.Spawner.args = ['--NotebookApp.allow_origin=*', '--NotebookApp.port=8888']
